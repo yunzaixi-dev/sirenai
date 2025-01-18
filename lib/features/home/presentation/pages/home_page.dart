d8b69adc45f49f529fe28a5ed8ecc38bd6111c88
yunzaixi-dev <x@zaixi.dev>
2025-01-18 21:16:41 +0800

    :recycle: Redirect main page to login interface

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sirenai/features/auth/presentation/pages/login_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Home Page'),
-      ),
-    );
+    return LoginPage();
   }
 }
